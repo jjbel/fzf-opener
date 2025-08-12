c687c007e09b9bbe01cb3823e61bad8185c0cbb7
Jai Bellare <jaibellare@gmail.com>
2025-08-12 21:21:50 +0530

    find.py: improvr fzf colors, fix history order

--- find.py
+++ find.py
@@ -8,8 +8,10 @@ base_path = argv[1].rstrip("/")
 finder_path = Path(__file__).parent / "find.py"
 history_path = Path(__file__).parent / "history.txt"
 
+fzf_opts = 'fzf --color=fg:#d0d0d0,fg+:#d0d0d0,bg:#101011,bg+:#262626   --color=hl:#5f87af,hl+:#22fff0,info:#afaf87,marker:#87ff00 --color=prompt:#d7005f,spinner:#af5fff,pointer:#af5fff,header:#87afaf --color=border:#262626,label:#aeaeae,query:#d9d9d9   --preview-window="border-rounded" --prompt="> " --marker=">"'
+
 p1 = Popen(["python", "list.py", base_path], stdout=PIPE, shell=True)
-p2 = Popen(["fzf"], stdin=p1.stdout, stdout=PIPE, shell=True)
+p2 = Popen(fzf_opts, stdin=p1.stdout, stdout=PIPE, shell=True)
 
 # Close p1's stdout in parent to allow fzf to receive EOF properly
 p1.stdout.close()
@@ -23,7 +25,7 @@ found_path = out.rstrip().decode()
 if found_path == "":
     exit()
 
-history_path.write_text(history_path.read_text() + str(found_path) + "\n")
+history_path.write_text(str(found_path) + "\n" + history_path.read_text())
 found_path = Path(base_path) / Path(found_path)
 # print(found_path)
 
